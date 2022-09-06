76a4bf720592e1ecf0a9984f6adad7732ac8b9cf
Nicolas J. Dowdy <njdowdy@gmail.com>
2022-09-06 15:33:55 -0500

    Initial commit.

---
+++ bioinformatics/bioinformatics/functions/augment.py
@@ -0,0 +1,9 @@
+# def create_aa_alignment()  # will require a function to calculate reading frame
+# def create_nt12_all_alignment()  # the data with only first and second codon positions included
+# def create_nt12_degen_alignment()  # the data which codes for only non-synonymous changes
+# def backtranslate() # convert AA to DNA (e.g., see tool, "Backtranseq" and "Backtranambig" @ https://www.ebi.ac.uk/Tools/st/)
+
+
+# remove_taxon_by_site_completeness()
+# remove_taxon_by_gene_completeness()
+# remove_site_by_taxon_completeness()
